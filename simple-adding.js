3085c40022a0b6c2f32d63976e62ad3bb1fa1320
Jessica Yin <jyin978@hotmail.com>
2018-12-14 20:35:06 -0800

    added another file

---
+++ simple-adding.js
@@ -0,0 +1,8 @@
+//add all num from 1 to given argument 
+function simpleAdding(num){
+    var answer = 0;
+    for (var i = 0; i <= num; i++){
+        answer = answer + i;
+    }
+    return answer; 
+}
